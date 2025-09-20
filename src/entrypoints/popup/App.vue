8a9a00bbd0828467eaac0956d3a8a670be292e11
Jaguar Zhou <ai.ourship@gmail.com>
2025-09-20 23:15:08 +0800

    feat: provide full screen button

--- src/entrypoints/popup/App.vue
+++ src/entrypoints/popup/App.vue
@@ -1,8 +1,22 @@
 <script lang="ts" setup>
+function openInFullscreen() {
+  browser.tabs.create({ url: "popup.html" });
+}
 </script>
 
 <template>
-  <div>
-    HEELO
+  <div class="flex items-center justify-center">
+    <div
+      class="relative flex h-120 w-150 flex-col rounded-sm outline-2"
+    >
+      <header class="flex items-center justify-end border-b border-primary p-1">
+        <button
+          class="btn btn-square btn-sm" @click="openInFullscreen"
+        >
+          <span class="sr-only">Open in fullscreen</span>
+          <i class="i-lucide-fullscreen size-4" />
+        </button>
+      </header>
+    </div>
   </div>
 </template>
