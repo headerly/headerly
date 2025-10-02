5c68563f264b9880e8683ee401c75925e09345bb
Jaguar Zhou <ai.ourship@gmail.com>
2025-10-02 12:00:29 +0800

    feat: add profile empty state placeholder

--- src/entrypoints/popup/components/SelectedProfile.vue
+++ src/entrypoints/popup/components/SelectedProfile.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import type { HTMLAttributes } from "vue";
+import { computed } from "vue";
 import {
   AUTOCOMPLETE_APPEND_REQUEST_FIELDS,
   AUTOCOMPLETE_SET_AND_REMOVE_REQUEST_FIELDS,
 } from "@/entrypoints/popup/constants/header";
 import { cn } from "@/lib/utils";
 import { useProfilesStore } from "../stores/useProfilesStore";
+import InteractiveGridPattern from "./InteractiveGridPattern.vue";
 
 const { class: className } = defineProps<{
   class?: HTMLAttributes["class"];
@@ -22,105 +24,136 @@ function getAutocompleteList(list: string[], input: string) {
   }
   return list.filter(field => field.includes(input) && field !== input);
 }
+
+const empty = computed(() =>
+  profilesStore.selectedProfile.requestHeaderMods.length === 0
+  && profilesStore.selectedProfile.responseHeaderMods.length === 0,
+);
 </script>
 
 <template>
   <div
     :class="cn(profilesStore.selectedProfile.enabled ? 'opacity-100' : `
       opacity-50
-    `, className)"
+    `, 'h-full', className)"
   >
-    <fieldset
-      v-if="profilesStore.selectedProfile.requestHeaderMods.length"
-      class="fieldset w-full rounded-box border border-base-300 bg-base-200 p-4"
+    <div
+      v-if="empty" class="
+        relative grid size-full place-content-center place-items-center gap-2
+        overflow-hidden p-2
+      "
     >
-      <legend class="fieldset-legend text-base">
-        <label>
-          <input
-            type="checkbox" class="checkbox checkbox-sm"
-            :checked="profilesStore.selectedProfile.requestHeaderMods.every(mod => mod.enabled)"
-            :indeterminate="profilesStore.selectedProfile.requestHeaderMods.some(mod => mod.enabled)
-              && !profilesStore.selectedProfile.requestHeaderMods.every(mod => mod.enabled)"
-            @change="(e) => {
-              const checked = (e.target as HTMLInputElement).checked;
-              profilesStore.selectedProfile.requestHeaderMods.forEach(mod => {
-                mod.enabled = checked;
-              });
-            }"
-          >
-        </label>
-        Reuqest Headers
-        <button
-          class="btn btn-square btn-ghost btn-xs btn-primary"
-          @click="profilesStore.addRequestHeaderMod('set')"
-        >
-          <i class="i-lucide-cross size-4" />
-        </button>
-      </legend>
-      <div
-        v-for="mod in profilesStore.selectedProfile.requestHeaderMods"
-        :key="mod.id"
-        class="flex flex-col gap-1.5"
+      <i class="i-lucide-cross size-8 text-base-content" />
+      <p
+        class="
+          z-10 text-center text-xl font-medium tracking-tighter
+          whitespace-pre-wrap text-base-content
+        "
       >
-        <label
-          class="label flex"
-        >
-          <input
-            v-model="mod.enabled" type="checkbox"
-            class="checkbox checkbox-sm"
-          >
-          <label class="flex-1">
-            <datalist v-if="mod.operation === 'append'" :id="APPEND_REQUEST_LIST_ID">
-              <option
-                v-for="field in getAutocompleteList(AUTOCOMPLETE_APPEND_REQUEST_FIELDS, mod.name)"
-                :key="field"
-                :value="field"
-              />
-            </datalist>
-            <datalist v-else :id="SET_AND_REMOVE_REQUEST_LIST_ID">
-              <option
-                v-for="field in getAutocompleteList(AUTOCOMPLETE_SET_AND_REMOVE_REQUEST_FIELDS, mod.name)"
-                :key="field"
-                :value="field"
-              />
-            </datalist>
+        No data, please add any mods or filters first.
+      </p>
+      <InteractiveGridPattern
+        class="
+          inset-0 h-[150%] skew-y-12
+          [mask-image:radial-gradient(350px_circle_at_center,white,transparent)]
+        "
+      />
+    </div>
+    <div v-else class="w-full">
+      <fieldset
+        v-if="profilesStore.selectedProfile.requestHeaderMods.length"
+        class="
+          fieldset w-full rounded-box border border-base-300 bg-base-200 p-4
+        "
+      >
+        <legend class="fieldset-legend text-base">
+          <label>
             <input
-              v-model="mod.name"
-              type="text"
-              placeholder="Name"
-              class="input input-sm w-full text-base text-base-content"
-              :list="mod.operation === 'append' ? APPEND_REQUEST_LIST_ID : SET_AND_REMOVE_REQUEST_LIST_ID"
-              @input="(e) => {
-                // Although the HTTP standard considers header names to be case-insensitive,
-                // `chrome.declarativeNetRequest` will report an error
-                // when receiving a header name with uppercase characters.
-                mod.name = (e.target as HTMLInputElement).value.toLowerCase();
+              type="checkbox" class="checkbox checkbox-sm"
+              :checked="profilesStore.selectedProfile.requestHeaderMods.every(mod => mod.enabled)"
+              :indeterminate="profilesStore.selectedProfile.requestHeaderMods.some(mod => mod.enabled)
+                && !profilesStore.selectedProfile.requestHeaderMods.every(mod => mod.enabled)"
+              @change="(e) => {
+                const checked = (e.target as HTMLInputElement).checked;
+                profilesStore.selectedProfile.requestHeaderMods.forEach(mod => {
+                  mod.enabled = checked;
+                });
               }"
             >
           </label>
-          <label v-if="mod.operation !== 'remove'" class="flex-1">
+          Reuqest Headers
+          <button
+            class="btn btn-square btn-ghost btn-xs btn-primary"
+            @click="profilesStore.addRequestHeaderMod('set')"
+          >
+            <i class="i-lucide-cross size-4" />
+          </button>
+        </legend>
+        <div
+          v-for="mod in profilesStore.selectedProfile.requestHeaderMods"
+          :key="mod.id"
+          class="flex flex-col gap-1.5"
+        >
+          <label
+            class="label flex"
+          >
             <input
-              v-model="mod.value"
-              type="text"
-              placeholder="Value"
-              class="input input-sm text-base text-base-content"
+              v-model="mod.enabled" type="checkbox"
+              class="checkbox checkbox-sm"
             >
+            <label class="flex-1">
+              <datalist v-if="mod.operation === 'append'" :id="APPEND_REQUEST_LIST_ID">
+                <option
+                  v-for="field in getAutocompleteList(AUTOCOMPLETE_APPEND_REQUEST_FIELDS, mod.name)"
+                  :key="field"
+                  :value="field"
+                />
+              </datalist>
+              <datalist v-else :id="SET_AND_REMOVE_REQUEST_LIST_ID">
+                <option
+                  v-for="field in getAutocompleteList(AUTOCOMPLETE_SET_AND_REMOVE_REQUEST_FIELDS, mod.name)"
+                  :key="field"
+                  :value="field"
+                />
+              </datalist>
+              <input
+                v-model="mod.name"
+                type="text"
+                placeholder="Name"
+                class="input input-sm w-full text-base text-base-content"
+                :list="mod.operation === 'append' ? APPEND_REQUEST_LIST_ID : SET_AND_REMOVE_REQUEST_LIST_ID"
+                @input="(e) => {
+                  // Although the HTTP standard considers header names to be case-insensitive,
+                  // `chrome.declarativeNetRequest` will report an error
+                  // when receiving a header name with uppercase characters.
+                  mod.name = (e.target as HTMLInputElement).value.toLowerCase();
+                }"
+              >
+            </label>
+            <label v-if="mod.operation !== 'remove'" class="flex-1">
+              <input
+                v-model="mod.value"
+                type="text"
+                placeholder="Value"
+                class="input input-sm text-base text-base-content"
+              >
+            </label>
+            <button
+              class="btn btn-square btn-ghost btn-xs btn-error"
+              @click="profilesStore.deleteRequestHeaderMod(mod.id)"
+            >
+              <i class="i-lucide-x size-4" />
+            </button>
           </label>
           <button
-            class="btn btn-square btn-ghost btn-xs btn-error"
-            @click="profilesStore.deleteRequestHeaderMod(mod.id)"
+            class="btn ml-6.5 w-min whitespace-nowrap btn-soft btn-xs"
+            @click="profilesStore.switchRequestHeaderModOperation(mod.id)"
           >
-            <i class="i-lucide-x size-4" />
+            Operation: <span class="capitalize">{{ mod.operation }}</span>
+            <i class="i-lucide-refresh-cw size-3" />
           </button>
-        </label>
-        <button
-          class="btn ml-6.5 w-min whitespace-nowrap btn-soft btn-xs"
-          @click="profilesStore.switchRequestHeaderModOperation(mod.id)"
-        >
-          Operation: <span class="capitalize">{{ mod.operation }}</span>
-          <i class="i-lucide-refresh-cw size-3" />
-        </button>
-      </div>
-    </fieldset>
+        </div>
+      </fieldset>
+    </div>
   </div>
 </template>
