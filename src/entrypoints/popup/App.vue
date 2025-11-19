1e98cad581b11a44cb06c17a2683e09632a906d3
Jaguar Zhou <ai.ourship@gmail.com>
2025-11-19 22:58:52 +0800

    feat: enable component caching in RouterView for improved performance

--- src/entrypoints/popup/App.vue
+++ src/entrypoints/popup/App.vue
@@ -10,7 +10,11 @@ const settingsStore = useSettingsStore();
   <div
     class="flex h-120 w-150 items-center justify-center outline-2"
   >
-    <RouterView />
+    <RouterView v-slot="{ Component }">
+      <KeepAlive>
+        <component :is="Component" />
+      </KeepAlive>
+    </RouterView>
     <Toaster :theme="settingsStore.theme" rich-colors />
   </div>
 </template>
