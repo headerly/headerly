07dfcdf525148da8e0e2098e3c5a924708163d2d
Jaguar Zhou <ai.ourship@gmail.com>
2025-12-25 23:33:24 +0800

    types: type the `v-auto-animate` global directive

--- extension/src/entrypoints/popup/main.ts
+++ extension/src/entrypoints/popup/main.ts
@@ -1,13 +1,25 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import type { vAutoAnimate } from "@formkit/auto-animate/vue";
 import { autoAnimatePlugin } from "@formkit/auto-animate/vue";
 import { VueQueryPlugin } from "@tanstack/vue-query";
 import { createPinia } from "pinia";
-import { createApp } from "vue";
-import App from "./App.vue";
 import { router } from "./router";
+
 import "@fontsource-variable/inter/wght-italic.css";
 import "@fontsource-variable/inter";
 import "@fontsource-variable/jetbrains-mono";
 import "./index.css";
+
+
+declare module "vue" {
+  export interface GlobalDirectives {
+    // prefix with v
+    vAutoAnimate: typeof vAutoAnimate;
+  }
+}
+
 
 createApp(App)
   .use(router)
