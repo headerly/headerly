b497a4745d03c5e5b172faa60e8d2aba5c5630e5
Jaguar Zhou <ai.ourship@gmail.com>
2026-03-29 22:13:51 +0800

    chore: fix eslint error

--- extension/src/lib/utils.ts
+++ extension/src/lib/utils.ts
@@ -6,7 +6,8 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+const macRegex = /Macintosh;/;
 export function getModKey() {
-  const isMac = navigator.userAgent.match(/Macintosh;/);
+  const isMac = navigator.userAgent.match(macRegex);
   return isMac ? "⌘" : "Ctrl";
 }
