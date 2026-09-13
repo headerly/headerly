bd7ca49cd625f0e7c6aba6a14ecfca686384dcd2
Brian Zhou <brianzhou.dev@gmail.com>
2026-09-13 18:41:21 +0800

    chore: fix Vite warning by adding missing file extensions in import statements (#84)

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 import { defineConfig } from "vitepress";
 import llmstxt from "vitepress-plugin-llms";
 import extensionPackage from "../../extension/package.json" with { type: "json" };
-import { generateDocsSidebar } from "./sidebar";
+import { generateDocsSidebar } from "./sidebar.ts";
 
 export default defineConfig({
   cleanUrls: true,
